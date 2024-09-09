b8425054d80751fc48810e88cd88bba5cbdea6f1
Samuel <scastro@pao-mx.com>
2024-09-09 09:25:06 -0700

    Estate Module

--- estate/models/test_model.py
+++ estate/models/test_model.py
@@ -8,3 +8,62 @@ class TestModel(models.Model):
         string='Title',  
         required=True,
     )
+    description = fields.Text(
+        string="Description",
+
+    )
+    postcode = fields.Integer(
+       string="Postcode",
+    )
+    available_date = fields.Date(
+       string="Available From",
+       copy=False,
+    )
+    expected_price = fields.Float(
+       string= "Expected Price",
+       default=0.00,
+       required=True,
+    )
+    selling_price = fields.Float(
+       string= "Selling Price",
+       default=0.00,
+       readonly=True,
+    )
+    beedrooms = fields.Integer(
+       string="Beedrooms",
+        default=2,
+    )
+    living_area = fields.Integer(
+       string="Living Area (sqm)",
+       default=0,
+    )
+    facades = fields.Integer(
+       string="Facades",
+       default=0,
+    )
+    garage = fields.Boolean(
+       string="Garage",
+       default=False,
+    )
+    garden = fields.Boolean(
+       string="Garden",
+       default=False,
+    )
+    garden_area = fields.Integer(
+       string="Garden Area (sqm)",
+       default=0,
+    )
+    active = fields.Boolean(
+       string="Active",
+       default=False,
+    )
+    status = fields.Selection(
+        selection=[
+            ('new', "New"),
+            ('offer_received', "Offer Received"),
+        ],
+        string="Status", 
+        default='new',
+        copy=False,
+        required=True,
+    )
