c0ab2fa5b361a9988333ee296faff24b337c5719
Samuel <scastro@pao-mx.com>
2024-09-09 16:34:32 -0700

    test

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Estate Account",  # The name that will appear in the App list
+    "version": "16.0.0",  # Version
+    "application": True,  # This line says the module is an App, and not a module
+    "depends": ["account",'estate'],  # dependencies
+    "data": [
+         #security
+        #'security/ir.model.access.csv',
+
+        #Views
+        #'views/estate_property_views.xml',
+
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+}
---
+++ estate_account/models/inherited_estate_property.py
@@ -0,0 +1,12 @@
+from odoo import models
+from logging import getLogger
+
+_logger = getLogger(__name__)
+
+class InheritedEstateProperty(models.Model):
+    _inherit = "test.model"
+
+    def action_sold(self):
+        res =  super(InheritedEstateProperty,self).action_sold()
+        _logger.error(res)
+        return res
