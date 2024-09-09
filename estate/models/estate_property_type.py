f0c212a1fe992f4e472c920f4ab64d3270e2881e
Samuel <scastro@pao-mx.com>
2024-09-09 16:15:17 -0700

    test

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,4 +1,5 @@
-from odoo import models,fields
+from odoo import models,fields,api
+from odoo.osv import expression
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
@@ -20,3 +21,24 @@ class EstatePropertyType(models.Model):
         string='Properties',
     )
     sequence = fields.Integer(string='Sequence', default=10)
+
+    offers_count = fields.Integer(compute='_compute_offers', string="Offers")
+
+    @api.depends('property_ids.offer_ids')
+    def _compute_sale_data(self):
+        for rec in self:
+            cont = 0
+            for l in rec.property_ids:
+                cont += len(l.offer_ids)
+            
+            rec.offers_count = cont
+
+
+    def action_view_offers(self):
+        self.ensure_one()
+        action = self.env["ir.actions.actions"]._for_xml_id("estate.property_offer_action")
+        action['context'] = {
+        }
+        action['domain'] = expression.AND([[('property_id', 'in', self.property_ids.ids)]])
+    
+        return action
