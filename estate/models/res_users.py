f28d03250a639c5e2c9c37cb72842d979dbade2d
Samuel <scastro@pao-mx.com>
2024-09-10 16:35:13 -0700

    test

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -4,6 +4,8 @@ from odoo import api, fields, models
 class Users(models.Model):
     _inherit = 'res.users'
 
-   
-    properties_ids = fields.One2many('test.model', 'salesman_id', string='Real Estate Properties',)
+    
+    def _get_domain(self):
+        return [('status', 'in', ['new', 'offer_received'])]
+    properties_ids = fields.One2many('test.model', 'salesman_id', string='Real Estate Properties', domain=_get_domain)
    
